b3b68bb14a82757c1639282f5c5cfe6ce26e33f5
Willy Gutsche <willy.gutsche@unifesp.br>
2022-11-20 16:39:47 -0300

    Implementing BoardException

---
+++ src/boardgame/exceptions/BoardException.java
@@ -0,0 +1,10 @@
+package boardgame.exceptions;
+
+public class BoardException extends RuntimeException {
+    
+    public static final long serialVersionUID = 1L;
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,7 +22,7 @@ public class ChessMatch {
         ChessPiece[][] matrix = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0 ; i < board.getRows() ; i++) {
             for (int j = 0 ; j < board.getColumns() ; j++) {
-                matrix[i][j] = (ChessPiece) board.piece(i, j);
+                matrix[i][j] = (ChessPiece)board.piece(i, j);
             }
         }
         return matrix;
