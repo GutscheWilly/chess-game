34d5cc6373fd3099cba07949174b8a60eaca163c
Willy Gutsche <willy.gutsche@unifesp.br>
2022-12-23 19:02:51 -0300

    Queen move

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -15,9 +16,66 @@ public class Queen extends ChessPiece {
         return "Q";
     }
 
+    private boolean validateMoveForQueen(Position position) {
+        return getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position));
+    }
+
+    private void applyDiagonalMoves(int gapRow, int gapColumn, boolean[][] possibleMoves) {
+        Position testMovePosition = new Position(position.getRow() + gapRow, position.getColumn() + gapColumn);
+
+        while (validateMoveForQueen(testMovePosition)) {
+            possibleMoves[testMovePosition.getRow()][testMovePosition.getColumn()] = true;
+
+            if (getBoard().thereIsAPiece(testMovePosition)) {
+                break;
+            }
+            
+            testMovePosition.setValues(testMovePosition.getRow() + gapRow, testMovePosition.getColumn() + gapColumn);
+        } 
+    }
+
+    private void applyHorizontalMoves(int gapColumn, boolean[][] possibleMoves) {
+        Position testPosition = new Position(position.getRow(), position.getColumn() + gapColumn);
+
+        while (validateMoveForQueen(testPosition)) {
+            possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+
+            if (getBoard().thereIsAPiece(testPosition)) {
+                break;
+            }
+
+            testPosition.setValues(testPosition.getRow(), testPosition.getColumn() + gapColumn);
+        }
+    }
+
+    private void applyVerticalMoves(int gapRow, boolean[][] possibleMoves) {
+        Position testPosition = new Position(position.getRow() + gapRow, position.getColumn());
+
+        while (validateMoveForQueen(testPosition)) {
+            possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+
+            if (getBoard().thereIsAPiece(testPosition)) {
+                break;
+            }
+
+            testPosition.setValues(testPosition.getRow() + gapRow, testPosition.getColumn());
+        }
+    }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        applyDiagonalMoves(-1, -1, possibleMoves);
+        applyDiagonalMoves(-1, 1, possibleMoves);
+        applyDiagonalMoves(1, -1, possibleMoves);
+        applyDiagonalMoves(1, 1, possibleMoves);
+
+        applyHorizontalMoves(1, possibleMoves);
+        applyHorizontalMoves(-1, possibleMoves);
+        applyVerticalMoves(1, possibleMoves);
+        applyVerticalMoves(-1, possibleMoves);
+
         return possibleMoves;
     }
 }
